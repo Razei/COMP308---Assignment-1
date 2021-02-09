1cb67bb4b6dce59427c80592a8d805b7152fcf76
Razei <razeixyz@gmail.com>
2021-02-09 00:33:03 -0500

    Added extra fields

--- app/controllers/thankYou.server.controller.js
+++ app/controllers/thankYou.server.controller.js
@@ -5,16 +5,18 @@ exports.render = function (req, res) {
         const courseCode = req.body.courseCode;
         const courseName = req.body.courseName;
         const comments = req.body.comments;
+        const favouriteTopic = req.body.favouriteTopic;
+        const favouriteAssignment = req.body.favouriteAssignment;
 
         //make a reference to the session object
         let session = req.session;
 
         //store the username in session object
-        session = Object.assign(session, {email, courseCode, courseName, comments});
+        // session = Object.assign(session, {email, courseCode, courseName, comments, favouriteTopic, favouriteAssignment});
         console.log("username in session: " + session.username);
     
         //show the display.ejs page and pass username to it
-        res.render("thankYou", {email, courseCode, courseName, comments});
+        res.render("thankYou", {email, courseCode, courseName, comments, favouriteTopic, favouriteAssignment});
     } else {
         res.render("index", { loginMessage: "Please login" });
     }
